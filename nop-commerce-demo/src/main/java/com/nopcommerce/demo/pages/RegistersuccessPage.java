c8fe4036453e6a0494d917e4c7e30a898dbbc000
kukii77 <100643358+kukii77@users.noreply.github.com>
2022-05-11 10:48:03 +0100

    Update RegistersuccessPage.java

--- nop-commerce-demo/src/main/java/com/nopcommerce/demo/pages/RegistersuccessPage.java
+++ nop-commerce-demo/src/main/java/com/nopcommerce/demo/pages/RegistersuccessPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class RegistersuccessPage extends Utility {
-    public void inIt(){
+    public RegistersuccessPage(){
         PageFactory.initElements(driver,this);
     }
     @FindBy(xpath = "//div[@class='result']")
