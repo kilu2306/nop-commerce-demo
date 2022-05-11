4023c93f0e0189e02d214de3c5a0109951da022b
kukii77 <100643358+kukii77@users.noreply.github.com>
2022-05-11 10:45:31 +0100

    Update DesktopPage.java

--- nop-commerce-demo/src/main/java/com/nopcommerce/demo/pages/DesktopPage.java
+++ nop-commerce-demo/src/main/java/com/nopcommerce/demo/pages/DesktopPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public class DesktopPage extends Utility {
 
-    public void inIt() {
+    public  DesktopPage() {
         PageFactory.initElements(driver, this);
 
     }
