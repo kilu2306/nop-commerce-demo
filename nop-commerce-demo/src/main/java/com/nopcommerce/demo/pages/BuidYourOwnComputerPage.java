ce7123ee1ad583e45e181f9f8c268f48ebc6f2ac
kukii77 <100643358+kukii77@users.noreply.github.com>
2022-05-11 10:44:23 +0100

    Update BuidYourOwnComputerPage.java

--- nop-commerce-demo/src/main/java/com/nopcommerce/demo/pages/BuidYourOwnComputerPage.java
+++ nop-commerce-demo/src/main/java/com/nopcommerce/demo/pages/BuidYourOwnComputerPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 import java.util.List;
 
 public class BuidYourOwnComputerPage extends Utility {
-   public void inIt(){
+   public BuidYourOwnComputerPage(){
        PageFactory.initElements(driver,this);
    }
     @FindBy(xpath = "//h1[contains(text(),'Build your own computer')]")
